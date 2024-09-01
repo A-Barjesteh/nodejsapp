9c652e02522e640720a0b1e0821b358335ab2fd1
A-Barjesteh <abarjesteh@gmail.com>
2024-08-31 18:14:29 -0700

    Update to display image on about page

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var http = require('http'); 
+var fs = require("fs")
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response
-  res.end(); //end the response
-}).listen(3000, function(){
- console.log("server start at port 3000"); //the server object listens on port 3000
+fs.readFile("lol.jpg", function(err,data) {
+  if (err) throw err;
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'}) //http header
+
+    var url = req.url
+
+    if(url==='/about'){
+      res.writeHead(200, {'Content-Type': 'image/jpeg'})
+      //res.write('<h1>about us page<h1>'); 
+      res.end(data);
+    }
+    else if(url==='/contact'){
+      res.write('<h1>contact us page<h1>');
+      res.end();
+    }
+    else{
+      res.write('Hello World!'); //write a response
+      res.end(); //end the response
+    }
+  }).listen(3000, function(){
+  console.log("server start at port 3000"); //the server object listens on port 3000
+  });
 });
